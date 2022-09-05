1f46ff3b8e8dbe250d3adbc169c42934f949ab8d
farah <sbshimul000@gmail.com>
2022-09-05 15:59:52 +0600

    issue fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,5 +30,7 @@ class AppServiceProvider extends ServiceProvider
         Validator::extend('old_password_check', function($attributes, $value){
             return Hash::check($value,Auth::user()->password);
         }, 'বর্তমান পাসওয়ার্ডটি সঠিক নয়');
+
+        Schema::defaultStringLength(191);
     }
 }
